66a2fa2a82e09cf7027c720c7af893c4ae0563ed
Chiara <csabato@unc.edu>
2021-11-13 03:12:00 -0500

    3:11

--- api_flights.py
+++ api_flights.py
@@ -7,7 +7,7 @@ url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/pric
 querystring = {"origin":"HKT","page":"None","currency":"RUB","destination":"-"}
 
 headers = {
-    'x-access-token': "341353",
+    'x-access-token': "",
     'x-rapidapi-host': "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
     'x-rapidapi-key': "9795c52b28msh004fe88c964bc24p1899c5jsn2b42c38742e3"
     }
