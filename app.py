89c50664c33e0b328bb6c5be250051a41fd59c54
Chiara <csabato@unc.edu>
2021-11-13 03:59:50 -0500

    .

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request
 
 from helpers import budget_to_location
@@ -6,6 +7,7 @@ app: Flask = Flask(__name__)
 desination: str = ""
 user_number: int = 0
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -13,13 +15,17 @@ def index():
 @app.route('/take_off', methods=["GET", "POST"])
 def take_off():
     if request.method == "POST":
-        global desination
+        global destination
         budget: str = request.form['budget']
         location: str = request.form['location']
         desination = budget_to_location(budget, location)
-        return render_template("take_off.html")
+        return render_template(f"{desination}.html")
+    return render_template("take_off.html")
 
-
+@app.route('/your_desination')
+def final_desination():
+    global desination
+    return render_template(f"{desination}.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
