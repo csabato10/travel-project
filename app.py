c84823edd12d5298e3dc681ae25019b90c411a89
Chiara <csabato@unc.edu>
2021-11-14 12:28:22 -0500

    extra pages and links

--- app.py
+++ app.py
@@ -22,6 +22,84 @@ def take_off():
         return render_template(f"{desination}.html")
     return render_template("take_off.html")
 
+@app.route('/beach', methods=["GET", "POST"])
+def beach():
+    if request.method == "POST":
+        return render_template("beach.html")
+    return render_template("beach.html")
+
+@app.route('/city', methods=["GET", "POST"])
+def city():
+    if request.method == "POST":
+        return render_template("city.html")
+    return render_template("city.html")
+
+@app.route('/wilderness', methods=["GET", "POST"])
+def wilderness():
+    if request.method == "POST":
+        return render_template("wilderness.html")
+    return render_template("wilderness.html")
+
+@app.route('/wilmington', methods=["GET", "POST"])
+def wilmington():
+    if request.method == "POST":
+        return render_template("wilmington.html")
+    return render_template("wilmington.html")
+
+@app.route('/miami', methods=["GET", "POST"])
+def miami():
+    if request.method == "POST":
+        return render_template("miami.html")
+    return render_template("miami.html")
+
+@app.route('/cape_town', methods=["GET", "POST"])
+def cape_town():
+    if request.method == "POST":
+        return render_template("cape_town.html")
+    return render_template("cape_town.html")
+
+@app.route('/charlotte', methods=["GET", "POST"])
+def charlotte():
+    if request.method == "POST":
+        return render_template("charlotte.html")
+    return render_template("charlotte.html")
+
+@app.route('/new_york', methods=["GET", "POST"])
+def new_york():
+    if request.method == "POST":
+        return render_template("new_york.html")
+    return render_template("new_york.html")
+
+@app.route('/dubai', methods=["GET", "POST"])
+def dubai():
+    if request.method == "POST":
+        return render_template("dubai.html")
+    return render_template("dubai.html")
+
+@app.route('/boone', methods=["GET", "POST"])
+def boone():
+    if request.method == "POST":
+        return render_template("boone.html")
+    return render_template("boone.html")
+
+@app.route('/yosemite', methods=["GET", "POST"])
+def yosemite():
+    if request.method == "POST":
+        return render_template("yosemite.html")
+    return render_template("yosemite.html")
+
+@app.route('/ooty', methods=["GET", "POST"])
+def ooty():
+    if request.method == "POST":
+        return render_template("ooty.html")
+    return render_template("ooty.html")
+
+@app.route('/', methods=["GET", "POST"])
+def return_home():
+    if request.method == "POST":
+        return render_template("index.html")
+    return render_template("index.html")
+
 @app.route('/your_desination')
 def final_desination():
     global desination
